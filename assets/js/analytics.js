9780f3919972e74973acc6fe93308ac882b0a203
staydrippedmobileiv <matthew@sabacompanyusa.com>
2025-06-19 04:15:21 -0700

    Update analytics.js

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,1 +1,8 @@
-
+// Lightweight Google Analytics helper
+(function(){
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-XXXXXXXX');
+  window.gtag = gtag;
+})();
